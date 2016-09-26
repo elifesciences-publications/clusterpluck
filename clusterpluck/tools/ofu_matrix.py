e34d3eadac9c9174eed65d6949c272007a8e8b55
Robin Shields-Cutler <cutler.robin@gmail.com>
2016-09-26 17:26:07 -0500

    working on args

--- clusterpluck/tools/ofu_matrix.py
+++ clusterpluck/tools/ofu_matrix.py
@@ -21,7 +21,7 @@ def make_arg_parser():
 	parser.add_argument('-o', '--output', help='Where to save the output csv; default to screen', required=False, default='-')
 	parser.add_argument('-a', '--annotate', help='Annotate the OFU table with NCBI tid, RefSeq Accession, and organism name', action='store_true', default=False)
 	parser.add_argument('-c', '--clusterme', help='If a percent identity scores matrix is provided, this will also perform hierarchical clustering', action='store_true', required=False, default=False)
-	parser.add_argument('-t', '--height', help='If clustering, at what height to cut the tree', required=False, default=0.3, dtype=float)
+	parser.add_argument('-t', '--height', help='If clustering, at what height to cut the tree', required=False, default=0.3, type=float)
 	return parser
 
 
