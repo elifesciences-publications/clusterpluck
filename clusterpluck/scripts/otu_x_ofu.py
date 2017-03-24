443d1e920f1357237d9340d7a9256f496fd0c4a0
Robin Shields-Cutler <cutler.robin@gmail.com>
2017-03-24 09:33:33 -0500

    Added multiprocessing support

--- clusterpluck/scripts/otu_x_ofu.py
+++ clusterpluck/scripts/otu_x_ofu.py
@@ -4,6 +4,9 @@ import argparse
 import pandas as pd
 import csv
 import os
+from itertools import repeat
+from multiprocessing import Pool
+from multiprocessing import cpu_count
 
 # The arg parser
 def make_arg_parser():
@@ -20,128 +23,141 @@ def make_arg_parser():
 						help='Save the matching OFU profiles csv as "matching_ofu_profiles.csv" in current working directory',
 						action='store_true', required=False, default=False)
 	parser.add_argument('--biom', help='Also convert the final OFU table to biom format, compatible with QIIME, etc. Output must end with ".txt".', action='store_true', required=False, default=False)
+	parser.add_argument('-c', '--cpus', help='Number of processors to use', required=False)
 	return parser
 
 
 # Uses the taxon table to refine the OFU profile according to taxons that were actually found
-def match_tables(taxon_file, ofu_infile, intax, ofu_index, opt):
+def match_tables(taxon_file, ofu_infile, intax, ofu_index, opt, cpus):
 	if taxon_file.endswith('.csv'):
-		tdf = pd.read_csv(intax, header=0, index_col=0)
+		tdf = pd.read_csv(intax, header=0, index_col=0, usecols=[0, 1, 2])
 	if taxon_file.endswith('.txt'):
-		tdf = pd.read_csv(intax, sep='\t', header=0, index_col=0)
+		tdf = pd.read_csv(intax, sep='\t', header=0, index_col=0, usecols=[0, 1, 2])
 	# odf = pd.read_csv(inofu, header=0, index_col=0)
 	taxons = list(tdf.index)
 	# ofu_index = list(odf.columns)
-	ofu_matched = pd.DataFrame(index=ofu_index)
+	# ofu_matched = pd.DataFrame(index=ofu_index)
 	# TODO: Parallelize the taxon parsing?
-	for taxon in taxons:
-		taxon = str(taxon)
-		if len(taxon.split(';')) < 3:
-			pass
-		# print(taxon)
-		n = []
-		t_odf = pd.DataFrame(columns=ofu_index)
-		taxon = str(taxon)
-		if taxon.endswith('None') or taxon.endswith('__'):
-			k = -2
-			name = taxon.split(';')[k]
-			# name = name.replace('s__', '')
-		else:
-			k = -1
-			name = taxon.split(';')[k]
-			# name = name.replace('t__', '')
-		with open(ofu_infile, 'r') as inofu:
-			# print(k, name)
-			ofu_reader = csv.reader(inofu)
-			for line in ofu_reader:
-				# print(line[0])
-				# print(name)
-				if name in line[0]:
-					# print('a match!')
-					line_df = pd.DataFrame([line[1:]], columns=ofu_index, index=[line[0]], dtype='int')
-					t_odf = t_odf.append(line_df)
-				else:
-					pass
-			if t_odf.empty and len(taxon.split(';')) >= 4:
-				up_name = taxon.split(';')[k - 1]
-				# print(k, up_name, 'up one')
-				with open(ofu_infile, 'r') as inofu:
-					ofu_reader = csv.reader(inofu)
-					for line in ofu_reader:
-						if up_name in line[0]:
-							# print('a second order match!')
-							line_df = pd.DataFrame([line[1:]], columns=ofu_index, index=[line[0]], dtype='int')
-							t_odf = t_odf.append(line_df)
-						else:
-							pass
-			if t_odf.empty and len(taxon.split(';')) >= 5:
-				up_name = taxon.split(';')[k - 2]
-				if 'k__' or 'p__' in up_name:
-					pass
-				# print(k, up_name, 'up two')
-				with open(ofu_infile, 'r') as inofu:
-					ofu_reader = csv.reader(inofu)
-					for line in ofu_reader:
-						if up_name in line[0]:
-							print('a third order match!')
-							line_df = pd.DataFrame([line[1:]], columns=ofu_index, index=[line[0]], dtype='int')
-							t_odf = t_odf.append(line_df)
-						else:
-							pass
-			if t_odf.empty and len(taxon.split(';')) >= 6:
-				up_name = taxon.split(';')[k - 3]
-				if 'k__' or 'p__' in up_name:
-					pass
-				# print(k, up_name, 'up three')
-				with open(ofu_infile, 'r') as inofu:
-					ofu_reader = csv.reader(inofu)
-					for line in ofu_reader:
-						if up_name in line[0]:
-							print('a fourth order match!')
-							line_df = pd.DataFrame([line[1:]], columns=ofu_index, index=[line[0]], dtype='int')
-							t_odf = t_odf.append(line_df)
-						else:
-							pass
+	args_list = [ofu_index, ofu_infile, opt]
+	with Pool(processes=cpus) as pool:
+		results = pool.starmap(parallel_taxon_match, zip(taxons, repeat(args_list)))
+		pool.close()
+		pool.join()
+		ofu_matched = pd.concat(results, axis=1, join='outer')  # stack all the results into a single column in a dataframe
+	ofu_matched = ofu_matched.T  # This orients the dataframe in the same way as a taxon table
+	return ofu_matched
 
-		# t_odf = odf.filter(like=name, axis=0)
-		if t_odf.empty:
-			# print('none here')
-			pass
-		elif sum(t_odf.sum()) == 0:
-			pass
-		elif opt == 'average':  # If resolution isn't to strain, then average the OFU counts across the higher-rank group (i.e. species)
-			mean = pd.DataFrame(t_odf.mean(axis=0))
-			n.append(taxon)
-			mean.columns = n
-			ofu_matched = ofu_matched.join(mean)
-		elif opt == 'summarize':  # Same as above, but here just take the summary, or the maximum number of appearances for any given OFU
-			summ = pd.DataFrame(t_odf.max(axis=0))
-			n.append(taxon)
-			summ.columns = n
-			ofu_matched = ofu_matched.join(summ)
-		elif opt == 'universal':  # Same as above, but here just take the minimum set, or only OFUs shared by all strains
-			univ = pd.DataFrame(t_odf.min(axis=0))
-			n.append(taxon)
-			univ.columns = n
-			ofu_matched = ofu_matched.join(univ)
-		elif opt == 'majority':  # Same as above, but here just take the majority set, or only OFUs shared by at least half of the strains
-			avgm = pd.DataFrame(t_odf.mean(axis=0))
-			dfbool = t_odf > 0
-			maj = pd.DataFrame(dfbool.mean(axis=0))
-			maj = maj >= 0.5
-			maj = maj * avgm
-			n.append(taxon)
-			maj.columns = n
-			ofu_matched = ofu_matched.join(maj)
-		else:
-			print('\nMust enter a valid method for dealing with multiple taxon-OFU hits, either -m "average" (default) or "summarize"\n')
-			quit()
-	if not ofu_matched.empty:
-		ofu_matched = ofu_matched.T  # This orients the dataframe in the same way as a taxon table
-		return ofu_matched
+
+def parallel_taxon_match(taxon, args_list):
+	ofu_index = args_list[0]
+	ofu_infile = args_list[1]
+	opt = args_list[2]
+	taxon = str(taxon)
+	if len(taxon.split(';')) < 3:
+		pass
+	# print(taxon)
+	n = []
+	t_odf = pd.DataFrame(columns=ofu_index)
+	taxon = str(taxon)
+	if taxon.endswith('None') or taxon.endswith('__'):
+		k = -2
+		name = taxon.split(';')[k]
 	else:
-		print('\nNo OTU matches found\n')
-		exit()
+		k = -1
+		name = taxon.split(';')[k]
+	with open(ofu_infile, 'r') as inofu:
+		# print(k, name)
+		ofu_reader = csv.reader(inofu)
+		for line in ofu_reader:
+			# print(line[0])
+			# print(name)
+			if name in line[0]:
+				# print('a match!')
+				line_df = pd.DataFrame([line[1:]], columns=ofu_index, index=[line[0]], dtype='int')
+				t_odf = t_odf.append(line_df)
+			else:
+				pass
+		if t_odf.empty and len(taxon.split(';')) >= 4:
+			up_name = taxon.split(';')[k - 1]
+			# print(k, up_name, 'up one')
+			with open(ofu_infile, 'r') as inofu:
+				ofu_reader = csv.reader(inofu)
+				for line in ofu_reader:
+					if up_name in line[0]:
+						# print('a second order match!')
+						line_df = pd.DataFrame([line[1:]], columns=ofu_index, index=[line[0]], dtype='int')
+						t_odf = t_odf.append(line_df)
+					else:
+						pass
+		if t_odf.empty and len(taxon.split(';')) >= 5:
+			up_name = taxon.split(';')[k - 2]
+			if 'k__' or 'p__' in up_name:
+				pass
+			# print(k, up_name, 'up two')
+			with open(ofu_infile, 'r') as inofu:
+				ofu_reader = csv.reader(inofu)
+				for line in ofu_reader:
+					if up_name in line[0]:
+						# print('a third order match!')
+						line_df = pd.DataFrame([line[1:]], columns=ofu_index, index=[line[0]], dtype='int')
+						t_odf = t_odf.append(line_df)
+					else:
+						pass
+		if t_odf.empty and len(taxon.split(';')) >= 6:
+			up_name = taxon.split(';')[k - 3]
+			if 'k__' or 'p__' in up_name:
+				pass
+			# print(k, up_name, 'up three')
+			with open(ofu_infile, 'r') as inofu:
+				ofu_reader = csv.reader(inofu)
+				for line in ofu_reader:
+					if up_name in line[0]:
+						# print('a fourth order match!')
+						line_df = pd.DataFrame([line[1:]], columns=ofu_index, index=[line[0]], dtype='int')
+						t_odf = t_odf.append(line_df)
+					else:
+						pass
+
+	# t_odf = odf.filter(like=name, axis=0)
+	if t_odf.empty:
+		# print('none here')
+		return None
+	elif sum(t_odf.sum()) == 0:
+		pass
+	elif opt == 'average':  # If resolution isn't to strain, then average the OFU counts across the higher-rank group (i.e. species)
+		mean = pd.DataFrame(t_odf.mean(axis=0))
+		n.append(taxon)
+		mean.columns = n
+		ofu_matched = mean
+	elif opt == 'summarize':  # Same as above, but here just take the summary, or the maximum number of appearances for any given OFU
+		summ = pd.DataFrame(t_odf.max(axis=0))
+		n.append(taxon)
+		summ.columns = n
+		ofu_matched = summ
+	elif opt == 'universal':  # Same as above, but here just take the minimum set, or only OFUs shared by all strains
+		univ = pd.DataFrame(t_odf.min(axis=0))
+		n.append(taxon)
+		univ.columns = n
+		ofu_matched = univ
+	elif opt == 'majority':  # Same as above, but here just take the majority set, or only OFUs shared by at least half of the strains
+		avgm = pd.DataFrame(t_odf.mean(axis=0))
+		dfbool = t_odf > 0
+		maj = pd.DataFrame(dfbool.mean(axis=0))
+		maj = maj >= 0.5
+		maj = maj * avgm
+		n.append(taxon)
+		maj.columns = n
+		ofu_matched = maj
+	else:
+		print('\nMust enter a valid method for dealing with multiple taxon-OFU hits, either -m "average" (default) or "summarize"\n')
+		quit()
+	if not ofu_matched.empty:
+		return ofu_matched
+
+
+	# else:
+	# 	print('\nNo OTU matches found\n')
+	# 	exit()
 
 
 # Use the relative abundances to create OFU table with 'abundance' of each OFU trait
@@ -173,6 +189,10 @@ def main():
 	args = parser.parse_args()
 
 	# Parse command line
+	if args.cpus:
+		cpus = int(args.cpus)
+	else:
+		cpus = cpu_count()
 	with open(args.taxons, 'r') as intaxon, open(args.ofus, 'r') as inofu:
 		taxon_file = args.taxons
 		if taxon_file.endswith('.csv'):
@@ -185,7 +205,7 @@ def main():
 	opt = args.multiples
 	with open(args.taxons, 'r') as intax:
 		ofu_infile = args.ofus
-		ofu_matched = match_tables(taxon_file, ofu_infile, intax, ofu_index, opt)
+		ofu_matched = match_tables(taxon_file, ofu_infile, intax, ofu_index, opt, cpus)
 	print('Finished matching OFUs from organisms in taxon table... now multiplying for abundance...\n')
 	if args.profiles:
 		profile_out = 'matching_ofu_profiles.csv'
