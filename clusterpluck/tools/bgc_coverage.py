96f35b91ace37059d5c01f01f6c7495da991e06e
Robin Shields-Cutler <cutler.robin@gmail.com>
2017-11-27 14:40:31 -0600

    Progress on code outline

--- clusterpluck/tools/bgc_coverage.py
+++ clusterpluck/tools/bgc_coverage.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import sys
 from collections import defaultdict
+from multiprocessing import cpu_count
 
 import pandas as pd
 from ninja_utils.parsers import FASTA
@@ -14,12 +15,15 @@ from clusterpluck.tools.suppress_print import suppress_stdout
 def make_arg_parser():
 	parser = argparse.ArgumentParser(description='Given the processed shotgun reads, measure coverage of predicted BGCs in metagenome')
 	parser.add_argument('-s', '--seqs', help="The combined seqs file (fasta) containing QC'd shotgun reads", required=True)
-	parser.add_argument('-d', '--bgc_fna', help="The BGC DNA fasta file (each BGC linearized then all BGCs in one multi-fasta file)", required=True)
-	parser.add_argument('-b', '--bread', help='Where to find the header for the sequence (default="ref|,|")', default='ref|,|')
+	parser.add_argument('--bgc_fna', help="The BGC DNA fasta file (all BGCs in one multi-fasta file)", required=False)
+	parser.add_argument('--bgc_edx', help="The pre-made BURST database file)", required=False)
+	parser.add_argument('--bgc_acx', help="The pre-made BURST accelerator file)", required=False)
+	# parser.add_argument('-b', '--bread', help='Where to find the header for the sequence (default="ref|,|")', default='ref|,|')
 	parser.add_argument('-o', '--output', help='Directory in which to save the results (default = cwd)', required=False, default='.')
 	parser.add_argument('-c', '--ofu', help='Comma-separated list of the ofus (e.g. ofu00001,ofu00003) on which to provide information', required=False, type=str)
-	parser.add_argument('--nt_cat', help='Path to the nt catalog, required for genbank ID clusters (i.e. antismash DB)', required=False, default='-')
+	parser.add_argument('--nt_cat', help='Path to the nt catalog, required for genbank ID clusters (i.e. antismash DB)', required=False)
 	parser.add_argument('-t', '--threshold', help='Coverage threshold at which to accept a cluster alignment (default = 75%)', required=False, default=75, type=float)
+	parser.add_argument('-p', '--threads', help='Number of processors to use for alignment (default = all available)', required=False, default='-', type=int)
 	return parser
 
 
@@ -50,18 +54,32 @@ def create_bgc_db(bgc_db, temp_path):
 	return acx, edx
 
 
-def align_bgcs(seqs, acx, edx):
-	alignment = os.path.join(output, 'bgc_alignments.b6')
-	os.system(' '.join(['burst -q', seqs, '-a', acx, '-f -n -r', edx, '-o', alignment]))
+def align_bgcs(outpath, seqs, acx, edx, cpus):
+	alignment = os.path.join(outpath, 'bgc_alignments.b6')
+	if acx != '-':
+		os.system(' '.join(['burst -q', seqs, '-a', acx, '-f -n -r', edx, '-o', alignment, '-t', cpus]))
+	else:
+		os.system(' '.join(['burst -q', seqs, '-f -n -r', edx, '-o', alignment, '-t', cpus]))
+	print("BURST alignment completed")
 	return alignment
+
+
+def coverage_analysis(alignment, outpath):
+	alignment_t = pd.read_table(alignment, header=None, index_col=0)
+	# TODO: Coverage... or just use shogun for the alignment and coverage?
+	coverage_result = do_stuff_to_it()
+	return coverage_result
 
 
 def main():
 	parser = make_arg_parser()
 	args = parser.parse_args()
 	outpath = args.output
-	bgc_db = args.bgc_fna
 	seqs = args.seqs
+	if args.threads == '-':
+		cpus = cpu_count()
+	else:
+		cpus = int(args.threads)
 	if not os.path.isfile(seqs):
 		raise ValueError('Sequences file not found')
 	threshold = float(args.threshold)
@@ -71,8 +89,26 @@ def main():
 			raise ValueError('Error creating output directory; check given path and try again')
 	temp_path = os.path.join(outpath, 'tempz')
 	os.mkdir(temp_path)
-	acx, edx = create_bgc_db(bgc_db, temp_path)
-	alignment = align_bgcs(seqs, acx, edx)
+	if args.bgc_fna:
+		bgc_db = args.bgc_fna
+		acx, edx = create_bgc_db(bgc_db, temp_path)
+	elif args.bgc_edx:
+		edx = args.bgc_edx
+		if args.bgc_acx:
+			acx = args.bgc_acx
+		else:
+			acx = '-'
+	elif not args.bgc_fna and not args.bgc_edx:
+		raise ValueError('No reference sequences or database provided: please include something to align to')
+
+	# Generate the alignment between sequences and BGCs
+	alignment = align_bgcs(outpath, seqs, acx, edx, cpus)
+
+	# TODO: Filter the alignment by predicted profiles?
+	# Or just see what is there?
+
+	coverage_result = coverage_analysis(alignment, outpath)
+
 
 if __name__ == '__main__':
 	main()
