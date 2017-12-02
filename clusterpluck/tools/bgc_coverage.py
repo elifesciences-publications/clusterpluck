51230ef872826dc08630e1aff19aedbaf7796e3a
Robin Shields-Cutler <cutler.robin@gmail.com>
2017-12-02 15:34:58 -0600

    Updated for burst12 and low-id alignment

--- clusterpluck/tools/bgc_coverage.py
+++ clusterpluck/tools/bgc_coverage.py
@@ -21,6 +21,7 @@ def make_arg_parser():
 	parser.add_argument('--bgc_acx', help="The pre-made BURST accelerator file)", required=False)
 	# parser.add_argument('-b', '--bread', help='Where to find the header for the sequence (default="ref|,|")', default='ref|,|')
 	parser.add_argument('-o', '--output', help='Directory in which to save the results (default = cwd)', required=False, default='.')
+	parser.add_argument('-m', '--metrics', help='Coverage metrics to report (default = all)', required=False, default='-')
 	parser.add_argument('-c', '--ofu', help='Comma-separated list of the ofus (e.g. ofu00001,ofu00003) on which to provide information', required=False, type=str)
 	parser.add_argument('--nt_cat', help='Path to the nt catalog, required for genbank ID clusters (i.e. antismash DB)', required=False)
 	parser.add_argument('-t', '--threshold', help='Coverage threshold at which to accept a cluster alignment (default = 75%)', required=False, default=75, type=float)
@@ -41,12 +42,12 @@ def make_arg_parser():
 
 
 # Make a database if necessary
-def create_bgc_db(bgc_db, temp_path):
+def create_bgc_db(bgc_db, temp_path, burst):
 	acc = os.path.join(temp_path, 'bgc_db.acc')
 	edb = os.path.join(temp_path, 'bgc_db.edb')
-	os.system(' '.join(['burst15 -r', bgc_db, '-a', acc, '-o', edb, '-d DNA -s']))
-	os.system(' '.join(['burst15 -a', acc, '-o /dev/null']))
-	os.system(' '.join(['burst15 -r', edb, '-o /dev/null']))
+	os.system(' '.join([burst, '-r', bgc_db, '-a', acc, '-o', edb, '-d DNA -s']))
+	os.system(' '.join([burst, '-a', acc, '-o /dev/null']))
+	os.system(' '.join([burst, '-r', edb, '-o /dev/null']))
 	os.remove(acc)
 	os.remove(edb)
 	acx = os.path.join(temp_path, 'bgc_db.acx')
@@ -60,19 +61,20 @@ def create_bgc_db(bgc_db, temp_path):
 def align_bgcs(outpath, seqs, acx, edx, cpus):
 	alignment = os.path.join(outpath, 'bgc_alignments.b6')
 	if acx != '-':
-		os.system(' '.join(['burst15 -m ALLPATHS -fr -q', seqs, '-n -r', edx, '-o', alignment, '-t', cpus, '-i 0.9', '-a', acx]))
+		os.system(' '.join(['burst15 -m ALLPATHS -fr -hr -q', seqs, '-n -r', edx, '-o', alignment, '-t', cpus, '-i 0.9', '-a', acx]))
 	else:
-		os.system(' '.join(['burst15 -m ALLPATHS -fr -q', seqs, '-n -r', edx, '-o', alignment, '-t', cpus, '-i 0.9']))
+		os.system(' '.join(['burst15 -m ALLPATHS -fr -hr -q', seqs, '-n -r', edx, '-o', alignment, '-t', cpus, '-i 0.9']))
 	print("BURST alignment completed")
 	return alignment
-	# TODO: use ALLPATHS
 
-def coverage_analysis(alignment, outpath):
+
+def coverage_analysis(alignment, outpath, output_metrics):
 	alignment_t = pd.read_table(alignment, header=None, index_col=0)
 	# TODO: Coverage... or just use shogun for the alignment and coverage?
 	np.array()
 	coverage_result = os.path.join(outpath, 'coverage_result.txt')
-	do_stuff_to_it()
+	# Run the allpaths coverage parsing script with desired output parameters
+	os.system(' '.join(['allpaths_are_belong_to_us', alignment, coverage_result, output_metrics]))
 	return coverage_result
 
 
@@ -85,6 +87,10 @@ def main():
 		cpus = cpu_count()
 	else:
 		cpus = int(args.threads)
+	if not args.metrics == '-':
+		output_metrics = str(args.metrics)
+	else:
+		args.metrics = 'all'
 	if not os.path.isfile(seqs):
 		raise ValueError('Sequences file not found')
 	threshold = float(args.threshold)
@@ -95,8 +101,13 @@ def main():
 	temp_path = os.path.join(outpath, 'tempz')
 	os.mkdir(temp_path)
 	if args.bgc_fna:
+		fna_bytes = int(os.path.getsize(args.bgc_fna))
+		if fna_bytes < 1000000000:
+			burst = 'burst12'
+		else:
+			burst = 'burst15'
 		bgc_db = args.bgc_fna
-		acx, edx = create_bgc_db(bgc_db, temp_path)
+		acx, edx = create_bgc_db(bgc_db, temp_path, burst)
 	elif args.bgc_edx:
 		edx = args.bgc_edx
 		if args.bgc_acx:
@@ -112,7 +123,7 @@ def main():
 	# TODO: Filter the alignment by predicted profiles?
 	# Or just see what is there?
 
-	coverage_result = coverage_analysis(alignment, outpath)
+	coverage_result = coverage_analysis(alignment, outpath, output_metrics)
 
 
 if __name__ == '__main__':
